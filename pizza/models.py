e094c497815a1fc5c9c650306bbf91ee94428e5f
Rafe <stefanorafe@gmail.com>
2023-01-29 01:15:47 +0100

    adds price

--- pizza/models.py
+++ pizza/models.py
@@ -10,6 +10,7 @@ class Size(models.Model):
 
 class Topping(models.Model):
     name =  models.CharField(max_length=10)
+    topping_price = models.DecimalField(max_digits=5, decimal_places=2)
     def __str__(self):
         return self.name
 
@@ -22,8 +23,8 @@ class Pizza(models.Model):
     address = models.CharField(max_length=200)
     created = models.DateTimeField(auto_now=True)
     completed = models.BooleanField(default=False)
-    price = models.DecimalField(max_digits=5, decimal_places=2)
+    pizza_price = models.DecimalField(max_digits=5, decimal_places=2, blank=True, null=True)
 
     def __str__(self):
         toppings = ', '.join(str(v) for v in self.topping.all())
-        return f'{self.size.name} - {toppings} - {self.customer_name} - {self.address}'
+        return f'{self.size.name} - {toppings} - {self.customer_name} - {self.address} - {self.pizza_price}'
--- pizza/views.py
+++ pizza/views.py
@@ -1,3 +1,4 @@
+import decimal
 from django.shortcuts import render
 from .forms import PizzaForm
 from django.contrib import messages
@@ -13,7 +14,30 @@ def order(request):
     if request.method == 'POST':
         form = PizzaForm(request.POST)
         if form.is_valid():
-            pizza = form.save()
+            pizza = form.save(commit=False)
+            print('inside valid')
+            size = form.cleaned_data.get('size')
+            print(size)
+            pizza_size_price = 4
+            if size == 'Medium':
+                pizza_size_price = 5.5
+            elif size == 'Large':
+                pizza_size_price = 7
+
+            topping_list = form.cleaned_data.get('topping')
+            print(topping_list)
+            topping_prices = 0
+            for topping in topping_list:
+                print(topping.topping_price)
+                print(topping_prices)
+                topping_prices += topping.topping_price
+
+            
+            print(pizza.pizza_price)
+            pizza.pizza_price = pizza_size_price + float(topping_prices)
+
+            pizza.save()
+
             pizza_pk = pizza.id
             messages.success(request, 'Your order is on the way!')
             form = PizzaForm()
