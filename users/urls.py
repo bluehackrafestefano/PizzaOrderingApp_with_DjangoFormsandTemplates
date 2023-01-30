8cc7ea903c0be7b0780b6297edddb81eabb036b0
Rafe <stefanorafe@gmail.com>
2023-01-30 21:25:44 +0100

    adds register without auto authentication

--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path, include
+from .views import register
 
 
 urlpatterns = [
     path('auth/', include('django.contrib.auth.urls')),
+    path('register/', register, name='register'),
 ]
--- users/views.py
+++ users/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+from django.shortcuts import redirect
 
-# Create your views here.
+
+def register(request):
+    form = UserCreationForm()
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Registered Successfully!!')
+            return redirect('login')
+    context = {
+        'form': form,
+    }
+    return render(request, 'registration/register.html', context)
